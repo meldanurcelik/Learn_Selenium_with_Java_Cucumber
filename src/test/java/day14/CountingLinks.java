dd4e15a617c7437ae48dc2cc347b9b3bb2109388
Meldanur Çelik <melda_celik@hotmail.com>
2022-01-26 22:01:58 +0300

    Added AlertsDemo, CountingLinks, FramesDemo, WaitsDemo

---
+++ src/test/java/day14/CountingLinks.java
@@ -0,0 +1,38 @@
+package day14;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class CountingLinks {
+
+    WebDriver driver;
+
+    @Test
+    public void main() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(); //Launch the chrome browser
+        driver.manage().window().maximize(); // Maximize your browser/page
+
+        driver.get("https://testautomationpractice.blogspot.com/");
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        System.out.println(links.size());
+
+        //reading all the links from web page and display
+
+        for (WebElement e : links) {
+            System.out.println(e.getText());
+        }
+
+        driver.quit();
+
+    }
+}
---
+++ src/test/java/day14/FramesDemo.java
@@ -0,0 +1,40 @@
+package day14;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FramesDemo {
+
+	WebDriver driver;
+
+	@Test
+	public void main() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver(); //Launch the chrome browser
+		driver.manage().window().maximize(); // Maximize your browser/page
+
+		driver.get("https://seleniumhq.github.io/selenium/docs/api/java/index.html");
+
+		driver.findElement(By.cssSelector("ul.navList:nth-child(1) li:nth-child(1) > a:nth-child(1)")).click();
+
+		driver.switchTo().frame("packageListFrame");
+		driver.findElement(By.linkText("org.openqa.selenium")).click(); //1 frame
+
+		driver.switchTo().defaultContent(); // go back to page/focus on the page
+
+		driver.switchTo().frame("packageFrame");
+		driver.findElement(By.linkText("WebDriver")).click(); //2 frame
+
+		driver.switchTo().defaultContent(); // go back to page/focus on the page
+
+		driver.switchTo().frame("classFrame");
+		driver.findElement(By.xpath("/html/body/header/nav/div[1]/div[1]/ul[1]/li[6]")).click(); // 3rd frame
+
+		driver.quit();
+
+	}
+}
